fc4e33d8ede82b85f5655a0284b08b0652ee29e9
kuvaan16 <kovannerway41@gmail.com>
2023-03-21 15:10:42 +0300

    user data

---
+++ lib/user_data_model.dart
@@ -0,0 +1,17 @@
+class User {
+  String name;
+  String email;
+  String password;
+  String phone;
+
+  User({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.phone,
+  });
+}
+
+List<User> userdata = [
+  User(name: "kuvaan16",email: "kovaan16@gmail.com",password: "kuvaan16",phone: "0750722")
+];
