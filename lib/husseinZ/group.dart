44b371fb079126536f8e6d9dd4fccf7c26852b8d
HUse1N7 <husseinzebari3@gmail.com>
2023-03-22 00:41:05 +0300

    group edit number 2

--- lib/husseinZ/group.dart
+++ lib/husseinZ/group.dart
@@ -51,7 +51,8 @@ class GroupPage extends StatelessWidget {
                     Text("Discover"),
                     SizedBox(width: 15,),
                     
-                    ]),
+                    ]
+                    ),
                 ),
               ),
                 SizedBox(width: 15,),
@@ -68,13 +69,192 @@ class GroupPage extends StatelessWidget {
                     Text("Create"),
                     SizedBox(width: 15,),
                     
-                    ]),
+                    ]
+                    ),
+                ),
+              )
+            ],
+            ),
+          SizedBox(height: 15,),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 150,
+                height: 130,
+               child: Column(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  child: Image.asset("assets/gambol (1).jpg")),
+                SizedBox(height: 5,),
+            Text("Gambol world",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,letterSpacing: 1),)
+              ],
+               ),
+                ),
+              ),
+              SizedBox(width: 15,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 150,
+                height: 130,
+               child: Column(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  child: Image.asset("assets/heads.jpg")),
+                SizedBox(height: 5,),
+            Text("Crazy world",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,letterSpacing: 1),)
+              ],
+               ),
+                ),
+              ),
+               SizedBox(width: 15,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 150,
+                height: 130,
+               child: Column(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  child: Image.asset("assets/jefrry.jpg")),
+                SizedBox(height: 5,),
+            Text("Cartoon Time",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,letterSpacing: 1),)
+              ],
+               ),
                 ),
               )
 
 
+              ],
+            ),
+          ),
+          Divider(thickness: 10,color: Colors.grey,),
+        SizedBox(height: 5,),
+        ListTile(
+          leading: Text("You've Been Invited",
+          style: TextStyle(fontWeight: FontWeight.bold,fontSize:20 ),),
+        ),
+        SizedBox(height: 5,),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            elevation: 6,
+            child: Container(
+              height: 250,
+              width: 500,
+              color: Colors.white,
+              child: Column(
+              children: <Widget>[
+              SizedBox(width: 15,),
+                ListTile(
+                  leading: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    child: Image.asset("assets/space.jpg")),
+                    title: Column(
+                    children: <Widget>[
+                      Text("#Black Spase",
+                    style: TextStyle(
+                      color: Colors.black,fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      ),),
+                      Text("Hussein has invited you !")
+                    ],
+                    ),
+                ),
+              SizedBox(height: 15,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 80,
+                          width: 500,
+                          color:Colors.grey[300],
+                        child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 5,),
+                        Text("Darwen",
+                        style: TextStyle(fontWeight:FontWeight.bold,
+                      fontSize: 18   
+                         ),),
+                        SizedBox(height: 5,),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text("Darwen , Gambol's brother in a cartoon showd in tv "),
+                        )
+                      ],
+                        ),
+                ),
+              ),
+              SizedBox(height: 5,),
+              Row(
+                children: <Widget>[
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                     child: Container(
+                      width: 150,
+                      height: 40,
+                      color: Colors.blue,
+                      child: Center(child: Text("Join",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 18
+                        ),)),
+                     ),
+                   ),
+                 ),
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                     child: Container(
+                      width: 150,
+                      height: 40,
+                      color: Colors.grey[300],
+                      child: Center(child: Text("Delet",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 18
+                        ),)),
+                     ),
+                   ),
+                 )
+                 
+                ],
+              )
+              ],
+              ),
+            ),
+          ),
+        ),
+        Divider(thickness: 10,color: Colors.grey,),
+        SizedBox(height: 15,),
+        ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            child: CircleAvatar(
+            radius: 25,
+            backgroundImage: AssetImage("assets/gambol(1).jpg"),
+            ),
+          ),
+          title: Row(
+            children: <Widget>[
+              Text("GAMBOL",style: TextStyle(fontWeight: FontWeight.bold),),
+              Icon(Icons.play_arrow),
+              Text("JEFRRY",style: TextStyle(fontWeight: FontWeight.bold),),
             ],
-            )
+          ),
+          trailing: Icon(Icons.menu),
+        )
           ],
         ),
       ),
