0749a67060c7af0e1a9751fb14b6240a304ac94f
kuvaan16 <kovannerway41@gmail.com>
2023-03-24 00:50:19 +0300

    notification screen

--- lib/kuvan/notfication.dart
+++ lib/kuvan/notfication.dart
@@ -2,6 +2,7 @@ import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class NotficationPage extends StatefulWidget {
@@ -19,14 +20,17 @@ class _NotficationPageState extends State<NotficationPage> {
         ListTile(
           leading: Text(
             "Notification",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
           trailing: CircleAvatar(
-              backgroundColor: Colors.grey[300],
-              child: Icon(
-                Icons.search,
-                color: Colors.black,
-              )),
+            radius: 18,
+            backgroundColor: Colors.grey[300],
+            child: Icon(
+              Icons.search,
+              color: Colors.black,
+              size: 30,
+            ),
+          ),
         ),
         SizedBox(
           height: 5,
@@ -38,21 +42,38 @@ class _NotficationPageState extends State<NotficationPage> {
             ),
             Text(
               "Earlier",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
             ),
           ],
         ),
+        SizedBox(
+          height: 15,
+        ),
         ListTile(
           leading: CircleAvatar(
+            radius: 35,
             backgroundImage: AssetImage("assets/messi.jpg"),
           ),
-          title: Text("Someone has reacted to your post"),
+          title: Html(
+              data:
+                  "<p><b>Sami Akre, Hussain Zebari</b> and 1616 other reacted to your post in <b>Flutter Cours</b></p>",
+              style: {
+                "p": Style(
+                    fontSize: FontSize.large, fontWeight: FontWeight.normal),
+                "b": Style(
+                  fontWeight: FontWeight.bold,
+                ),
+              }),
+              
           trailing: Icon(
             FontAwesomeIcons.ellipsis,
             color: Colors.black,
             size: 18,
           ),
         ),
+        
+        Text("3 hours ago                                        ",
+            style: TextStyle(color: Colors.grey[600])),
       ],
     );
   }
