af222d4f84267403bf952eb1cc830c0dbc338177
kuvaan16 <kovannerway41@gmail.com>
2023-03-25 22:10:44 +0300

    homescreen finished

--- lib/kuvan/homescreen.dart
+++ lib/kuvan/homescreen.dart
@@ -3,6 +3,7 @@ import 'package:facebook/husseinZ/video.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../husseinZ/group.dart';
 import '../sami/profile.dart';
@@ -30,12 +31,23 @@ class _HomeScreenState extends State<HomeScreen> {
             "facebook",
             style: TextStyle(
               color: Colors.indigo,
-              fontWeight: FontWeight.bold,
+              fontWeight: FontWeight.w900,
               fontSize: 24,
             ),
           ),
           backgroundColor: Colors.white,
           actions: <Widget>[
+            CircleAvatar(
+              child: Icon(
+                Icons.add,
+                color: Colors.black,
+              ),
+              backgroundColor: Colors.grey[300],
+              radius: 20,
+            ),
+            SizedBox(
+              width: 10,
+            ),
             CircleAvatar(
               child: Icon(
                 Icons.search,
@@ -45,7 +57,7 @@ class _HomeScreenState extends State<HomeScreen> {
               radius: 20,
             ),
             SizedBox(
-              width: 15,
+              width: 10,
             ),
             CircleAvatar(
               child: Icon(
@@ -56,47 +68,45 @@ class _HomeScreenState extends State<HomeScreen> {
               radius: 20,
             ),
             SizedBox(
-              width: 10,
+              width: 5,
             ),
           ],
-          bottom: TabBar(tabs: <Widget>[
-            Tab(
-              icon: Icon(
-                Icons.home,
-                color: Colors.black,
+          bottom: TabBar(
+            labelColor: Colors.blue,
+            unselectedLabelColor: Colors.grey[500],
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(
+                  Icons.home,
+                ),
               ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.group_add,
-                color: Colors.black,
+              Tab(
+                icon: Icon(
+                  Icons.group_outlined,
+                ),
               ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.ondemand_video_rounded,
-                color: Colors.black,
+              Tab(
+                icon: Icon(
+                  Icons.ondemand_video_rounded,
+                ),
               ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.person_outline,
-                color: Colors.black,
+              Tab(
+                icon: Icon(
+                  Icons.person_outline,
+                ),
               ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.notifications_none_rounded,
-                color: Colors.black,
+              Tab(
+                icon: Icon(
+                  Icons.notifications_none_rounded,
+                ),
               ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.menu,
-                color: Colors.black,
+              Tab(
+                icon: Icon(
+                  Icons.menu,
+                ),
               ),
-            ),
-          ]),
+            ],
+          ),
         ),
         body: TabBarView(
           children: <Widget>[
